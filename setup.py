819e54e066447a0bcf9030a012182b9faf125e53
Adam Candy <adam.candy@imperial.ac.uk>
2013-08-14 12:18:55 +0100

    Add a Python setup.py file for installation.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='shingle',
+  version='1.1',
+  description='Generation of boundary representation from arbitrary geophysical fields.',
+  author='Adam Candy',
+  author_email='adam.candy@imperial.ac.uk',
+  license = "GNUv3",
+  url='https://github.com/adamcandy/Shingle',
+  packages = ['shingle'],
+  install_requires = ['numpy', 'scientific', 'matplotlib']
+)
+
