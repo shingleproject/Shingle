1ec50c889ab77e89540ca4bb2638db1710ece7f9
Adam Candy <adam.candy+github@gmail.com>
2017-04-20 14:04:27 +0200

    Release v2.0.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ libspud = Extension('libspud',
 #)
 
 setup(name='shingle',
-    version='2.0.1',
+    version='2.0.2',
     description='Generation of boundary representation and mesh spatial discretisations from arbitrary geophysical fields.',
     author = 'Adam S. Candy',
     author_email='contact@shingleproject.org',
